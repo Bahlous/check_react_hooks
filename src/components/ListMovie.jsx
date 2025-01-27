b42e1339650cafc1fd8f447c1370829c8803d703
Bahlous <josef.yassou2004@gmail.com>
2025-01-27 09:51:30 +0100

    Update ListMovie.jsx

--- src/components/ListMovie.jsx
+++ src/components/ListMovie.jsx
@@ -6,7 +6,7 @@ const ListMovie = ({movies, searchTitle, searchRate}) => {
   return (
     <div style={{display:'flex',justifyContent:'center', flexWrap:'wrap', backgroundColor:'black'}}>
     {movies
-    .filter((movie)=>movie.title.toUpperCase().includes(searchTitle.toUpperCase())&& movie.rate === searchRate)
+    .filter((movie)=>movie.title.toUpperCase().includes(searchTitle.toUpperCase())&& movie.rate >= searchRate)
   
     .map(movie=><CardMovie movie={movie} key={movie.id}/>)}
     </div>
